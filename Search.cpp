b68c4b7b414bb1a744426514b0a68c935cf8d03b
Jordan Adriano <56409204+jordanadrianoo@users.noreply.github.com>
2020-02-28 10:33:21 -0600

    Update Search.cpp

--- Search.cpp
+++ Search.cpp
@@ -1,6 +1,11 @@
-
+// Jordan Adriano
 // This program will search 2D vectors of floats and return the x and y coordinates
-//
+
+//Functions List
+// main()
+// Search  - will search the 2D vector for a given float and return a 2D vector of locations
+// display - will show what contents are in a 2D vector
+// display_x_and_y_locations - will show the X and Y coordinates of the locations found in search
 
 # include<iostream>
 # include<vector>
@@ -12,11 +17,12 @@ vector<vector<float>> search(vector<vector<float>> vect, float value)
 	vector<vector<float>> x;
 	vector<float> y;
 
+	cout << " Searching for all \""<<value<<"\"s in the 2D vector\n";
+
 	if (vect.size() != 0)
 	{
 		for (int i = 0; i < vect.size(); i++)
 		{
-			
 			for(int j=0; j<vect[i].size();j++)
 			{
 				if (vect[i][j] == value)
@@ -24,67 +30,72 @@ vector<vector<float>> search(vector<vector<float>> vect, float value)
 					y.push_back(j);
 				}
 			}
-
 			if (y.size() != 0)
 			{
 				x.push_back(y);
 				y.clear();
 			}
 		}
-
 	}
-
 	return x;
 }
 
 void display(vector<vector<float>> vect)
 {
 	
-
+	cout << " The vector contains: \n";
 	for (int i = 0; i < vect.size(); i++)
 	{
 		for (int j = 0; j < vect[i].size(); j++)
 		{
 			cout << vect[i][j] << " ";
-			
 		}
 		cout << endl;
-		
 	}
 	cout << endl;
 }
 
-void display_x_and_y_locations(vector<vector<float>> vect)
+void display_x_and_y_locations(vector<vector<float>> vect,float value)
 {
-
-	for (int i = 0; i < vect.size(); i++)
+	if (vect.size() != 0)
 	{
-		for (int j = 0; j < vect[i].size(); j++)
+		cout << " The X and Y coordinates found using " <<value<<".\n";
+		for (int i = 0; i < vect.size(); i++)
 		{
-			cout << "[ " << i << " , " << vect[i][j] << "] \n";
-
+			for (int j = 0; j < vect[i].size(); j++)
+			{
+				cout << "[ " << i << " , " << vect[i][j] << "] \n";
+			}
 		}
-		
-
+		cout << endl;
 	}
-	cout << endl;
-
+	else
+	{
+		cout << " sorry! no locations found!\n";
+	}
 }
 
 int main()
 {
+	float search_number = 0;
 	vector<vector<float>> positions{};
-	vector<vector<float>> nums{ {10,20,30,10,101},
-								{10,10,10,10,60},
-								{70,10,10,80,10,} };
-	cout << " The orginal vector is: \n";
+
+	vector<vector<float>> nums{ {10,20,80,10,101},
+								{80,10,10,10,60},
+								{80,10,10,80,10,} };
 	display(nums);
 
-	cout << " Searching for all \"10\"s in the 2D vecor\n";
-	positions=search(nums, 10); // will set the vector "positions" to all locations found
+	search_number = 10; // sets the number we will look for
+	positions=search(nums, search_number); // will set the 2D vector "positions" with all locations found
+	display_x_and_y_locations(positions, search_number); // will show the coordinates of the locations if found
 
-	cout << " The X and Y coordinates found using 10.";
-	display_x_and_y_locations(positions);
+	search_number = 80;
+	positions = search(nums, 80);
+	display_x_and_y_locations(positions,search_number);
+
+	search_number = 0;
+	positions = search(nums, search_number);
+	display_x_and_y_locations(positions, search_number);
 
 	system("pause");
 } 
