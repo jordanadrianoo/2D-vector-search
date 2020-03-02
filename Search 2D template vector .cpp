fd69ba1d5040f53d8e63d6c743e7461480fca7fc
Jordan Adriano <56409204+jordanadrianoo@users.noreply.github.com>
2020-03-02 07:21:32 -0600

    Update Search 2D template vector .cpp

--- Search 2D template vector .cpp
+++ Search 2D template vector .cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <vector>
 #include <list>
@@ -38,9 +37,61 @@ vector<vector<T>> search(vector<vector<T>> vect, T value)
 
 }
 
+template <class T>
+void display(vector<vector<T>> vect)
+{
+	if (vect.size() != 0)
+	{
+		cout << "The vector contents are..." << endl;
+		for (int i = 0; i < vect.size(); i++)
+		{
+			for (int j = 0; j < vect[i].size(); j++)
+			{
+				cout << vect[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+	else
+	{
+		cout << "The Vector is empty." << endl;
+	}
+}
 
 template <class T>
+void display_locations(vector<vector<T>> vect, T value)
+{
+	if (vect.size() != 0)
+	{
+		cout <<"size is"<< vect.size() << endl;
+		cout << "The locations of " << value << " were found at..." << endl;
+		cout << "[Y] [X]"<<endl;
+		for (int i = 0; i < vect.size(); i++)
+		{
+			for (int j = 0; j < vect[i].size(); j++)
+			{
+				cout << "[" << i << "] [" << vect[i][j] << "]" << endl;
+			}
+		}
+	}
+	else
+	{
+		cout << "Sorry! No Locations were found using " << value << endl;
+	}
+}
 
 int main()
 {
 	vector <vector<int>>nums = { { 3,2,3 }, {3, 5, 3}, {7, 3, 9} };
+	vector <vector<char>>letters = { { 'A','B','D' }, {'D', 'E', 'F','G'}, {'D', 'I', 'J'} };
+	vector <vector<double>>nums2 = { { 1.0,2.0,1.0 }, {4.0, 5.2, 1.0}, {7.4, 9.123} };
+	
+	display(nums);
+	display_locations(search(nums, 3),3);
+
+	display(letters);
+	display(search(letters, 'D'));
+
+	display(nums2);
+	display_locations(search(nums2, 1.11), 1.11);
+}
